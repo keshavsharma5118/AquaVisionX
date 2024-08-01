b16acf5c83a611bdb01cc2a2dff6e6cce4ba1b6c
keshavsharma5118 <keshav.s5118@gmail.com>
2024-08-02 02:04:19 +0530

    change commit
    removed scripts.js file

--- sidhu ref/script.js
+++ sidhu ref/script.js
@@ -18,4 +18,17 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// You can add custom markers or other map features here
+
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the form from submitting
+
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+
+    if (email && password) {
+        // Simulate a successful login by redirecting to a welcome page
+        window.location.href = 'welcome.html';
+    } else {
+        alert('Please enter both email and password.');
+    }
+});
